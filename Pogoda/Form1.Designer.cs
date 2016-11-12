b1ebfe6fc0613e12eb41c7e1a3a05196fa16410a
EvheniyKsenich <zzzzzzzzzzzz182@gmail.com>
2016-11-12 09:20:14 +0200

    1
    1

---
+++ Pogoda/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Pogoda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_temp = new System.Windows.Forms.Label();
+            this.label_prea = new System.Windows.Forms.Label();
+            this.label_w_sp = new System.Windows.Forms.Label();
+            this.label_volog = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_temp_water = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Погода";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_temp
+            // 
+            this.label_temp.AutoSize = true;
+            this.label_temp.Location = new System.Drawing.Point(89, 38);
+            this.label_temp.Name = "label_temp";
+            this.label_temp.Size = new System.Drawing.Size(0, 13);
+            this.label_temp.TabIndex = 2;
+            // 
+            // label_prea
+            // 
+            this.label_prea.AutoSize = true;
+            this.label_prea.Location = new System.Drawing.Point(92, 61);
+            this.label_prea.Name = "label_prea";
+            this.label_prea.Size = new System.Drawing.Size(0, 13);
+            this.label_prea.TabIndex = 3;
+            // 
+            // label_w_sp
+            // 
+            this.label_w_sp.AutoSize = true;
+            this.label_w_sp.Location = new System.Drawing.Point(92, 84);
+            this.label_w_sp.Name = "label_w_sp";
+            this.label_w_sp.Size = new System.Drawing.Size(0, 13);
+            this.label_w_sp.TabIndex = 4;
+            // 
+            // label_volog
+            // 
+            this.label_volog.AutoSize = true;
+            this.label_volog.Location = new System.Drawing.Point(89, 106);
+            this.label_volog.Name = "label_volog";
+            this.label_volog.Size = new System.Drawing.Size(0, 13);
+            this.label_volog.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Температура";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Шв_вітру";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Тиск";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Вологість";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Темп води";
+            // 
+            // label_temp_water
+            // 
+            this.label_temp_water.AutoSize = true;
+            this.label_temp_water.Location = new System.Drawing.Point(89, 131);
+            this.label_temp_water.Name = "label_temp_water";
+            this.label_temp_water.Size = new System.Drawing.Size(0, 13);
+            this.label_temp_water.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(177, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 187);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Валюта";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(153, 117);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 24;
+            this.label14.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(153, 95);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 13);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "label15";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(153, 72);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(41, 13);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "label16";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(153, 45);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 13);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "Продажа";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(69, 45);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 13);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Покупка";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(37, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Рубль";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(69, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "label10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(69, 95);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 95);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Євро";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Долар";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Валюта";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label_temp_water);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_volog);
+            this.Controls.Add(this.label_w_sp);
+            this.Controls.Add(this.label_prea);
+            this.Controls.Add(this.label_temp);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_temp;
+        private System.Windows.Forms.Label label_prea;
+        private System.Windows.Forms.Label label_w_sp;
+        private System.Windows.Forms.Label label_volog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_temp_water;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Pogoda/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pogoda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            string site = client.DownloadString("https://www.gismeteo.ua/weather-zhytomyr-4943/");
+            Regex temperature = new Regex(@"<dd class='value m_temp c'>((?:\+|-)?\d+)");
+            Regex vindSp = new Regex(@"<dd class='value m_wind ms' style='display:inline'>(\d+)");
+            Regex preas = new Regex(@"<dd class='value m_press torr'>(\d+)");
+            Regex vilig = new Regex(@"<div class=.+ title=.+>(\d+)");
+            Regex tw = new Regex(@"<div class=.wicon water. title=.+>\n\t\t<dd class=.value m_temp c[""]>((?:\+|-)?\d+)");
+
+            Match tmp = temperature.Match(site);
+            label_temp.Text = tmp.Groups[1].Value;
+
+            tmp = vindSp.Match(site);
+            label_w_sp.Text = tmp.Groups[1].Value +"м\\с";
+
+            tmp = preas.Match(site);
+            label_prea.Text = tmp.Groups[1].Value;
+
+            tmp = vilig.Match(site);
+            label_volog.Text = tmp.Groups[1].Value;
+
+            tmp = tw.Match(site);
+            label_temp_water.Text = tmp.Groups[1].Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            string site = client.DownloadString("http://finance.i.ua/");
+            Regex dolar = new Regex(@"<td class=.align_left.><b>USD<\/b><\/td>\n\t\t\t\t\t<td>\n\t\t\t\t<big>(\d+\.?\d+)");
+            Regex euro = new Regex(@"<td class=.align_left.><b>EUR<\/b><\/td>\n\t\t\t\t\t<td>\n\t\t\t\t<big>(\d+\.?\d+)");
+            Regex rub = new Regex(@"<td class=.align_left.><b>RUB<\/b><\/td>\n\t\t\t\t\t<td>\n\t\t\t\t<big>(\d+\.?\d+)");
+
+            Regex dolar1 = new Regex(@"<td class=.align_left.><b>USD<\/b><\/td>\s+<td>\s+<big>(\d+\.?\d+)<\/big><i class=.decrease.><\/i><br \/><span class=.report.>.+<\/span><\/td>\s+<td>\s+<big>(\d+\.?\d+)");
+            Regex euro1 = new Regex(@"<td class=.align_left.><b>EUR<\/b><\/td>\s+<td>\s+<big>(\d+\.?\d+)<\/big><i class=.decrease.><\/i><br \/><span class=.report.>.+<\/span><\/td>\s+<td>\s+<big>(\d+\.?\d+)");
+            Regex rub1 = new Regex(@"<td class=.align_left.><b>RUB<\/b><\/td>\s+<td>\s+<big>(\d+\.?\d+)<\/big><i class=.decrease.><\/i><br \/><span class=.report.>.+<\/span><\/td>\s+<td>\s+<big>(\d+\.?\d+)");
+
+            Match tmp = dolar.Match(site);
+            label3.Text = tmp.Groups[1].Value;
+
+            tmp = euro.Match(site);
+            label9.Text = tmp.Groups[1].Value;
+
+            tmp = rub.Match(site);
+            label10.Text = tmp.Groups[1].Value;
+
+
+            tmp = dolar1.Match(site);
+            label16.Text = tmp.Groups[2].Value;
+
+            tmp = euro1.Match(site);
+            label15.Text = tmp.Groups[2].Value;
+
+            tmp = rub1.Match(site);
+            label14.Text = tmp.Groups[2].Value;
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
